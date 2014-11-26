76ee7e746403068e64a8fe564fa904c05212e85d
Jeremy Morgan <jeremymorgan@gmail.com>
2014-11-26 11:04:29 -0800

    messedup

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -1,117 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using HtmlAgilityPack;
 
 namespace ConsoleTester
 {
     class Program
     {
-
-        static void createDictionary()
-        {
-            Dictionary<int, string> ourdictionary = new Dictionary<int,string>();
-
-            ourdictionary.Add(0, "1234");
-            ourdictionary.Add(1, "Unit");
-            ourdictionary.Add(2, "2014-11-25 21:39:07.000");
-            ourdictionary.Add(3, "2014-11-25 21:39:07.000");
-            ourdictionary.Add(4, "2014-11-25 21:39:07.000");
-            ourdictionary.Add(5, "2014-11-25 21:39:07.000");
-
-            Console.WriteLine(ourdictionary[0]);
-            Console.WriteLine(ourdictionary[1]);
-            Console.WriteLine(ourdictionary[2]);
-            Console.WriteLine(ourdictionary[3]);
-            Console.WriteLine(ourdictionary[4]);
-            Console.WriteLine(ourdictionary[5]);
-
-        }
-
-        static void createObject()
-        {
-            UnitTime ourUnitTime = new UnitTime();
-
-            ourUnitTime.callnum = 1234;
-            ourUnitTime.unitname = "Unit";
-            ourUnitTime.date1 = DateTime.Parse("2014-11-25 21:39:07.000");
-            ourUnitTime.date2 = DateTime.Parse("2014-11-25 21:39:07.000");
-            ourUnitTime.date3 = DateTime.Parse("2014-11-25 21:39:07.000");
-            ourUnitTime.date4 = DateTime.Parse("2014-11-25 21:39:07.000");
-
-            Console.WriteLine(ourUnitTime.callnum);
-            Console.WriteLine(ourUnitTime.unitname);
-            Console.WriteLine(ourUnitTime.date1);
-            Console.WriteLine(ourUnitTime.date2);
-            Console.WriteLine(ourUnitTime.date3);
-            Console.WriteLine(ourUnitTime.date4);
-        }
-
-        static void createStruct()
-        {
-            UnitTimeStruct ourStruct = new UnitTimeStruct();
-
-            ourStruct.callnum = 1234;
-            ourStruct.unitname = "Unit";
-            ourStruct.date1 = DateTime.Parse("2014-11-25 21:39:07.000");
-            ourStruct.date2 = DateTime.Parse("2014-11-25 21:39:07.000");
-            ourStruct.date3 = DateTime.Parse("2014-11-25 21:39:07.000");
-            ourStruct.date4 = DateTime.Parse("2014-11-25 21:39:07.000");
-
-            Console.WriteLine(ourStruct.callnum);
-            Console.WriteLine(ourStruct.unitname);
-            Console.WriteLine(ourStruct.date1);
-            Console.WriteLine(ourStruct.date2);
-            Console.WriteLine(ourStruct.date3);
-            Console.WriteLine(ourStruct.date4);
-
-        }
         static void Main(string[] args)
         {
 
-            // Create new stopwatch
-            Stopwatch stopwatch = new Stopwatch();
+            var webGet = new HtmlWeb();
+            // load wccca page
+            var document = webGet.Load("http://localhost/wccca/units.html");
 
-            // Begin timing
-            stopwatch.Start();
-    
-            // dictionary test
-            int runs = Int32.Parse(args[1]);
-            
-            for (int i = 0; i < runs; i++)
+            // Get all tables in the document
+            HtmlNodeCollection tables = document.DocumentNode.SelectNodes("//table");
+
+            // Iterate all rows in the first table
+            HtmlNodeCollection rows = tables[0].SelectNodes(".//tr");
+            for (int i = 0; i < rows.Count; ++i)
             {
+                Console.WriteLine("----------");
+                Console.WriteLine("Row: " + i);
+                // this is where we write new
 
-                switch (args[0])
+                // Iterate all columns in this row
+                HtmlNodeCollection cols = rows[i].SelectNodes(".//td");
+                for (int j = 0; j < cols.Count; ++j)
                 {
-                    case "dictionary":
-                        createDictionary();
-                        break;
-                    case "object":
-                        createObject();
-                        break;
-                    case "struct":
-                        createStruct();
-                        break;
+                    
+                    string value = cols[j].InnerText;
+                    Console.WriteLine(value);
                 }
-
             }
 
-            stopwatch.Stop();
+            Console.ReadLine();
 
-            // Get the elapsed time as a TimeSpan value.
-            TimeSpan ts = stopwatch.Elapsed;
-
-            // Format and display the TimeSpan value 
-            //Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
-
-            string fileName = args[0] + "-" + args[1] + ".txt";
-
-            using (var fs = new FileStream(fileName, FileMode.Append, FileAccess.Write))
-            using (var sw = new StreamWriter(fs))
-            {
-                sw.WriteLine("{0}", stopwatch.Elapsed);
-            }
-          
         }
     }
 }
